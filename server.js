a6277d28353bf0acc80f26a5bab804c421ed44ce
Anthony Rivas <kamikazebot@gmail.com>
2019-03-12 12:59:59 -0700

    preparing for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
 let app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json({}))
 app.use(express.urlencoded({ extended: false }))
